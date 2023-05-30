e9e66aa824e4f877b8623106880ae2a0c74b6394
DistoBot <distobot@distoai.com>
2023-05-30 09:00:22 -0400

    Add a user_id column to the external_service_repos table. This will allow us to directly associate repos with users, eliminating the need for the costly join operation.

--- internal/database/repos.go
+++ internal/database/repos.go
@@ -1030,10 +1030,7 @@ func (s *RepoStore) listSQL(ctx context.Context, opt ReposListOptions) (*sqlf.Qu
 }
 
 const userReposQuery = `
-SELECT repo_id as id
-FROM external_service_repos esr
-JOIN external_services es ON esr.external_service_id = es.id
-WHERE es.namespace_user_id = %d AND es.deleted_at IS NULL
+SELECT repo_id as id FROM external_service_repos WHERE user_id = %d
 `
 
 const userPublicReposQuery = `
